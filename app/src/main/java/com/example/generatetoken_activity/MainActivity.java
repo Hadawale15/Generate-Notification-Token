f2eae41e0f9a9ae0b732207b834fbc697329ff30
Hadawale15 <shubhamhadawale15@gmail.com>
2021-10-28 16:59:18 +0530

    first commit

---
+++ app/src/main/java/com/example/generatetoken_activity/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.generatetoken_activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.internal.FirebaseInstanceIdInternal;
+import com.google.firebase.installations.FirebaseInstallations;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    private FirebaseAuth mAuth;
+    public static final String NODE_USERS = "users";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.t1);
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (task.isSuccessful()){
+                    String Token=task.getResult();
+                    textView.setText("Token:"+Token);
+                    
+                    Toast.makeText(MainActivity.this, "Token Generated", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+}
